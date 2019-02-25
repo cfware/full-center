59b11763a9437f3b34fbe9c8042be83dbc22e905
Corey Farrell <git@cfware.com>
2019-02-25 14:10:13 -0500

    chore: Use @cfware/fastify-test-helper

--- test/helpers/setup-testing.js
+++ test/helpers/setup-testing.js
@@ -1,11 +1,5 @@
-import path from 'path';
-
 import {setup, page} from '@cfware/ava-selenium-manager';
-import fastify from 'fastify';
-import fastifyStatic from 'fastify-static';
-import fastifyBabel from 'fastify-babel';
-
-const rootDir = path.resolve(__dirname, '..', '..');
+import FastifyTestHelper from '@cfware/fastify-test-helper';
 
 page('full-center.html', async t => {
 	const {selenium, snapshotImage, grabImage} = t.context;
@@ -16,46 +10,9 @@ page('full-center.html', async t => {
 });
 
 export function setupTesting(browserBuilder) {
-	setup({
-		browserBuilder,
-		async daemonFactory() {
-			const daemon = fastify()
-				.register(fastifyStatic, {
-					root: rootDir,
-					serve: false
-				})
-				.register(fastifyStatic, {
-					root: path.resolve(rootDir, 'test', 'fixtures'),
-					prefix: '/',
-					decorateReply: false
-				})
-				.register(fastifyStatic, {
-					root: path.resolve(rootDir, 'node_modules'),
-					prefix: '/node_modules',
-					decorateReply: false
-				})
-				.register(fastifyBabel, {
-					babelrc: {
-						babelrc: false,
-						configFile: false,
-						plugins: [
-							'istanbul',
-							'bare-import-rewrite'
-						]
-					},
-					maskError: false
-				})
-				.get('/full-center.js', (_, reply) => reply.sendFile('full-center.js'));
-
-			await daemon.listen(0);
-
-			return daemon;
-		},
-		daemonStop(daemon) {
-			daemon.server.unref();
-		},
-		daemonGetURL(t, daemon, pathname) {
-			return `http://localhost:${daemon.server.address().port}/${pathname}`;
+	setup(new FastifyTestHelper(browserBuilder, {
+		customGetters: {
+			'/full-center.js': 'full-center.js'
 		}
-	});
+	}));
 }
